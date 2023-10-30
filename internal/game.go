7af17e58a3d9d3943f5f5e7d84e93bef8a89985f
ishi <berugen1st@gmail.com>
2023-10-31 06:20:27 +0700

    handle flapping upward logic

--- internal/game.go
+++ internal/game.go
@@ -2,6 +2,7 @@ package internal
 
 import (
 	"math"
+	"time"
 
 	r1 "github.com/gen2brain/raylib-go/raylib"
 )
@@ -16,15 +17,14 @@ type Game struct {
 }
 
 func (g *Game) Init() {
-	centered_x := WIDTH / 2
-	centered_y := 0
+	centered_x := (WIDTH / 2) - BIRD_SIZE
+	centered_y := (HEIGHT / 2) - BIRD_SIZE
 
-	g.Bird.Size = r1.Vector2{X: 25.0, Y: 25.0}
+	g.Bird.Size = r1.Vector2{X: float32(BIRD_SIZE), Y: float32(BIRD_SIZE)}
 	g.Bird.Position = r1.Vector2{X: float32(centered_x), Y: float32(centered_y)}
-	g.Bird.Speed = 0.5
+	g.Bird.Speed = 1.5
 	g.Bird.Color = r1.LightGray
-	g.Bird.Flapping = -5
-	g.Bird.Flapped = true
+	g.Bird.Flapping = 0.7
 	g.FrameCounter = 1.0
 
 	g.GameOver = false
@@ -39,19 +39,21 @@ func (g *Game) Update() {
 			g.FrameCounter = 0.0
 		}
 
-		t := math.Pow(float64(g.FrameCounter), 2)
-		distance := float32(0.001 * 9.8 * t)
-		originalSpeed := g.Bird.Speed
-		g.Bird.Position.Y += originalSpeed + distance
+		t := float32(math.Pow(float64(g.FrameCounter), 2))
+		distance := 0.5 * G * t
+		g.Bird.Position.Y += (g.Bird.Speed + distance) * 0.0010
 
 		if r1.IsKeyPressed(r1.KeyQ) {
 			g.GameOver = true
 		}
 
 		if r1.IsKeyPressed(r1.KeySpace) || r1.IsMouseButtonPressed(r1.MouseLeftButton) {
-			g.Bird.Speed = float32(g.Bird.Flapping)
-			g.Bird.Position.Y += originalSpeed
 			g.FrameCounter = 0.0
+
+			for i := 1.2; i < 12.0; i += 1.2 {
+				g.Bird.Position.Y += -(float32(i) * float32(g.Bird.Flapping))
+				time.Sleep(10 * time.Millisecond)
+			}
 		}
 
 		g.FrameCounter++
