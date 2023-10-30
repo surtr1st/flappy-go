ece75680d5cd0d2f5436fc25ec3e8abb2d063a77
ishi <berugen1st@gmail.com>
2023-10-31 06:20:03 +0700

    add gravity and bird size as constant

--- internal/constants.go
+++ internal/constants.go
@@ -2,7 +2,9 @@ package internal
 
 var (
 	TITLE      = "Flappy"
-	WIDTH      = int32(800)
-	HEIGHT     = int32(600)
+	WIDTH      = int32(500)
+	HEIGHT     = int32(800)
 	TARGET_FPS = int32(60)
+	G          = float32(9.8)
+	BIRD_SIZE  = int32(25)
 )
