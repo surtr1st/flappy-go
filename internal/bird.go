1b14b9653b9455fec6434028ecfc4cdd080d7fd7
ishi <berugen1st@gmail.com>
2023-10-31 00:22:16 +0700

    change type of speed

--- internal/bird.go
+++ internal/bird.go
@@ -5,8 +5,8 @@ import r1 "github.com/gen2brain/raylib-go/raylib"
 type Bird struct {
 	Size     r1.Vector2
 	Position r1.Vector2
-	Speed    r1.Vector2
 	Color    r1.Color
+	Speed    float32
 	Flapping int32
 	Flapped  bool
 }
