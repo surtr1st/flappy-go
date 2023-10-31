6aced52f129caedcb9b8e248e85b39c61e0c1b47
ishi <berugen1st@gmail.com>
2023-10-31 07:13:45 +0700

    change pipe structure

--- internal/pipe.go
+++ internal/pipe.go
@@ -3,7 +3,6 @@ package internal
 import rl "github.com/gen2brain/raylib-go/raylib"
 
 type Pipe struct {
-	Size     rl.Vector2
-	Position rl.Vector2
-	Color    rl.Color
+	Rec   rl.Rectangle
+	Color rl.Color
 }
