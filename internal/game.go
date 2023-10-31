7de7eb4b39b2779b306b11666a0d4c63c7365335
ishi <berugen1st@gmail.com>
2023-10-31 08:07:27 +0700

    handle collision dectection and speed increment

--- internal/game.go
+++ internal/game.go
@@ -1,6 +1,7 @@
 package internal
 
 import (
+	"fmt"
 	"math"
 	"time"
 
@@ -12,9 +13,11 @@ type Game struct {
 	Pipes []Pipe
 
 	Camera       rl.Camera2D
+	Score        int32
 	GameOver     bool
 	Pause        bool
 	FrameCounter float32
+	PipesPos     []rl.Vector2
 }
 
 func (g *Game) Init() {
@@ -29,27 +32,30 @@ func (g *Game) Init() {
 
 	g.Camera = rl.NewCamera2D(rl.NewVector2(float32(x), 0), rl.NewVector2(g.Bird.Position.X, g.Bird.Position.Y), 0.0, 1.0)
 
-	pipesPos := make([]rl.Vector2, MAX_PIPES)
+	g.PipesPos = make([]rl.Vector2, MAX_PIPES)
 	for i := 0; i < int(MAX_PIPES); i++ {
-		pipesPos[i].X = float32(480 + 360*i)
-		pipesPos[i].Y = -float32(rl.GetRandomValue(0, 240))
+		g.PipesPos[i].X = float32(480 + 360*i)
+		g.PipesPos[i].Y = -float32(rl.GetRandomValue(0, 240))
 	}
 
 	pipesWidth := 60
 	g.Pipes = make([]Pipe, MAX_PIPES*2)
 	for i := 0; i < int(MAX_PIPES)*2; i += 2 {
-		g.Pipes[i].Rec.X = pipesPos[i/2].X
-		g.Pipes[i].Rec.Y = pipesPos[i/2].Y
+		g.Pipes[i].Rec.X = g.PipesPos[i/2].X
+		g.Pipes[i].Rec.Y = g.PipesPos[i/2].Y
 		g.Pipes[i].Rec.Width = float32(pipesWidth)
 		g.Pipes[i].Rec.Height = 420
 		g.Pipes[i].Color = rl.Green
 
-		g.Pipes[i+1].Rec.X = pipesPos[i/2].X
-		g.Pipes[i+1].Rec.Y = 1200 + pipesPos[i/2].Y - 550
+		g.Pipes[i+1].Rec.X = g.PipesPos[i/2].X
+		g.Pipes[i+1].Rec.Y = 1200 + g.PipesPos[i/2].Y - 550
 		g.Pipes[i+1].Rec.Width = float32(pipesWidth)
 		g.Pipes[i+1].Rec.Height = 420
+
+		g.Pipes[i/2].Active = true
 	}
 
+	g.Score = 0
 	g.GameOver = false
 	g.Pause = false
 }
@@ -57,11 +63,11 @@ func (g *Game) Init() {
 func (g *Game) Update() {
 	if !g.GameOver {
 
+		isAtTop := g.Bird.Position.Y <= 0
 		isAtBottom := g.Bird.Position.Y >= float32(HEIGHT)-g.Bird.Size.Y
 
-		if isAtBottom {
-			g.Bird.Position.Y = float32(HEIGHT) - g.Bird.Size.Y
-			g.FrameCounter = 0.0
+		if isAtTop || isAtBottom {
+			g.GameOver = true
 		}
 
 		t := float32(math.Pow(float64(g.FrameCounter), 2))
@@ -76,7 +82,7 @@ func (g *Game) Update() {
 		if rl.IsKeyPressed(rl.KeySpace) || rl.IsMouseButtonPressed(rl.MouseLeftButton) {
 			g.FrameCounter = 0.0
 
-			jumpHeight := 12.2
+			jumpHeight := 12.5
 
 			for i := 1.2; i < jumpHeight; i += 1.2 {
 				g.Bird.Position.Y += -(float32(i) * float32(g.Bird.Flapping))
@@ -84,7 +90,32 @@ func (g *Game) Update() {
 			}
 		}
 
-		g.Bird.Position.X += 3.0
+		for i := 0; i < int(MAX_PIPES)*2; i++ {
+			if rl.CheckCollisionRecs(rl.NewRectangle(g.Bird.Position.X, g.Bird.Position.Y, float32(BIRD_SIZE), float32(BIRD_SIZE)), g.Pipes[i].Rec) {
+				g.GameOver = true
+			} else if (g.PipesPos[i/2].X < g.Bird.Position.X-float32(BIRD_SIZE)) && g.Pipes[i/2].Active && !g.GameOver {
+				// Score point
+				g.Score += 1
+				g.Pipes[i/2].Active = false
+			}
+		}
+
+		if g.Score >= 0 {
+			g.Bird.Position.X += 2.2
+		}
+
+		if g.Score >= 12 {
+			g.Bird.Position.X += 2.6
+		}
+
+		if g.Score >= 24 {
+			g.Bird.Position.X += 3.0
+		}
+
+		if g.Score >= 36 {
+			g.Bird.Position.X += 3.2
+		}
+
 		g.Camera.Target = rl.NewVector2(g.Bird.Position.X, 0)
 
 		g.FrameCounter++
@@ -102,12 +133,11 @@ func (g *Game) Draw() {
 	for i := 0; i < int(MAX_PIPES); i++ {
 		rl.DrawRectangle(int32(g.Pipes[i*2].Rec.X), int32(g.Pipes[i*2].Rec.Y), int32(g.Pipes[i*2].Rec.Width), int32(g.Pipes[i*2].Rec.Height), g.Pipes[i*2].Color)
 		rl.DrawRectangle(int32(g.Pipes[i*2+1].Rec.X), int32(g.Pipes[i*2+1].Rec.Y), int32(g.Pipes[i*2+1].Rec.Width), int32(g.Pipes[i*2+1].Rec.Height), g.Pipes[i*2].Color)
-
-		rl.DrawRectangleLines(int32(g.Pipes[i*2].Rec.X), int32(g.Pipes[i*2].Rec.Y), int32(g.Pipes[i*2].Rec.Width), int32(g.Pipes[i*2].Rec.Height), rl.Black)
-		rl.DrawRectangleLines(int32(g.Pipes[i*2+1].Rec.X), int32(g.Pipes[i*2+1].Rec.Y), int32(g.Pipes[i*2+1].Rec.Width), int32(g.Pipes[i*2+1].Rec.Height), rl.Black)
 	}
 
 	rl.EndMode2D()
+
+	rl.DrawText(fmt.Sprintf("%02d", g.Score), (WIDTH/2 - 1), 0, 32, rl.White)
 
 	rl.EndDrawing()
 }
