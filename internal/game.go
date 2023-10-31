dc4bec68d8b45d128d4291a90a66089045dcd3ae
ishi <berugen1st@gmail.com>
2023-10-31 07:13:54 +0700

    draw pipes

--- internal/game.go
+++ internal/game.go
@@ -26,6 +26,27 @@ func (g *Game) Init() {
 	g.Bird.Flapping = 0.7
 	g.FrameCounter = 1.0
 
+	pipesPos := make([]rl.Vector2, MAX_PIPES)
+	for i := 0; i < int(MAX_PIPES); i++ {
+		pipesPos[i].X = float32(480 + 360*i)
+		pipesPos[i].Y = -float32(rl.GetRandomValue(0, 240))
+	}
+
+	pipesWidth := 60
+	g.Pipes = make([]Pipe, MAX_PIPES*2)
+	for i := 0; i < int(MAX_PIPES)*2; i += 2 {
+		g.Pipes[i].Rec.X = pipesPos[i/2].X
+		g.Pipes[i].Rec.Y = pipesPos[i/2].Y
+		g.Pipes[i].Rec.Width = float32(pipesWidth)
+		g.Pipes[i].Rec.Height = 400
+		g.Pipes[i].Color = rl.Green
+
+		g.Pipes[i+1].Rec.X = pipesPos[i/2].X
+		g.Pipes[i+1].Rec.Y = 1200 + pipesPos[i/2].Y - 550
+		g.Pipes[i+1].Rec.Width = float32(pipesWidth)
+		g.Pipes[i+1].Rec.Height = 400
+	}
+
 	g.GameOver = false
 	g.Pause = false
 }
@@ -69,6 +90,14 @@ func (g *Game) Draw() {
 
 	rl.ClearBackground(rl.Color{R: 0, G: 0, B: 0, A: 1})
 	rl.DrawRectangleV(g.Bird.Position, g.Bird.Size, g.Bird.Color)
+
+	for i := 0; i < int(MAX_PIPES); i++ {
+		rl.DrawRectangle(int32(g.Pipes[i*2].Rec.X), int32(g.Pipes[i*2].Rec.Y), int32(g.Pipes[i*2].Rec.Width), int32(g.Pipes[i*2].Rec.Height), g.Pipes[i*2].Color)
+		rl.DrawRectangle(int32(g.Pipes[i*2+1].Rec.X), int32(g.Pipes[i*2+1].Rec.Y), int32(g.Pipes[i*2+1].Rec.Width), int32(g.Pipes[i*2+1].Rec.Height), g.Pipes[i*2].Color)
+
+		rl.DrawRectangleLines(int32(g.Pipes[i*2].Rec.X), int32(g.Pipes[i*2].Rec.Y), int32(g.Pipes[i*2].Rec.Width), int32(g.Pipes[i*2].Rec.Height), rl.Black)
+		rl.DrawRectangleLines(int32(g.Pipes[i*2+1].Rec.X), int32(g.Pipes[i*2+1].Rec.Y), int32(g.Pipes[i*2+1].Rec.Width), int32(g.Pipes[i*2+1].Rec.Height), rl.Black)
+	}
 
 	rl.EndDrawing()
 }
